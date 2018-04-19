404698a63240a626e80bac2364f740899b417668
Thiago Macedo <thiagodark1995@gmail.com>
2018-04-19 14:39:31 -0500

    teste

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MoovieProvider } from '../../providers/moovie/moovie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,19 +13,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+    MoovieProvider
+  ]
 })
 export class FeedPage {
-  public nome_usuario:string = "Thiago de Macedo Lima";
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  public objeto_feed = {
+    titulo: "Thiago Macedo",
+    data: "November 5, 1995",
+    descricao: "estou criando um app incrivel",
+    qntd_likes: 12,
+    qntd_comments: 4,
+    time_comments: "11g ago"
   }
 
-  public somaDoisNumeros(num1:number, num2:number):void {
-    alert(num1 + num2);
+  public lista_filmes = new Array<any>();
+
+  public nome_usuario: string = "Thiago de Macedo Lima";
+  constructor(private movieProvider: MoovieProvider, public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  public somaDoisNumeros(num1: number, num2: number): void {
+    //alert(num1 + num2);
   }
 
 
   ionViewDidLoad() {
-    //this.somaDoisNumeros(10,99);
+    this.movieProvider.getLatestMovies().subscribe(
+      data => {
+        const response = (data as any);
+        const objeto_retorno = JSON.parse(response._body);
+        this.lista_filmes = objeto_retorno.results;
+        console.log(objeto_retorno);
+      }, error => {
+        console.log(error);
+      }
+    )
   }
 
 }
